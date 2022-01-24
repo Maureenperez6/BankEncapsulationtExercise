8667b34dcc65758753a5775fd7c0326b819dba65
Maureen Perez <maureen8694@gmail.com>
2022-01-23 22:24:19 -0500

    initial Commit

---
+++ BankEncapsulationExercise/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulationExercise
+{
+    public class BankAccount
+    {
+        public BankAccount(double amount)
+        {
+            balance = amount;
+        }
+        private double balance = 0; 
+        public void Deposit(double amount)
+        {
+            balance += amount;
+
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+        
+    }
+            
+}
---
+++ BankEncapsulationExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BankEncapsulationExercise
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount myAccount = new BankAccount(600.00);
+            Console.WriteLine("New bank Account created");
+
+            Console.WriteLine($"Your balance is: {myAccount.GetBalance()}");
+
+        }
+    }
+}
+
